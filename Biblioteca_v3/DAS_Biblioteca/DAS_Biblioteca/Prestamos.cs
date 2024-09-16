ef6faddb31daa0c62619782522a2e4246f82dfa5
FrancoCartagena <francocartagena92@hotmail.com>
2024-09-15 23:45:56 -0600

    Add files via upload

---
+++ Biblioteca_v3/DAS_Biblioteca/DAS_Biblioteca/Prestamos.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace DAS_Biblioteca
+{
+    public class Prestamos
+    {
+        private Usuarios    User { get; set; }
+        private Libros      Libro { get; set; }
+        private DateTime FechaPrestamo { get; set; }
+        private DateTime FechaDevolucion { get; set; }
+
+        private bool PrestamoActivo { get; set; }
+
+        // Constructor general
+        public Prestamos(Usuarios us, Libros li, DateTime fp)
+        {
+            User = us;
+            Libro = li;
+            FechaPrestamo = fp;
+
+            PrestamoActivo = true;
+        }
+
+        public Usuarios GetUsuario()
+        {
+            return User;
+        }
+
+        public Libros GetLibro()
+        {
+            return Libro;
+        }
+
+        public DateTime GetFechaPrestamo()
+        { return FechaPrestamo; }
+
+        public bool GetPrestamoActivo()
+        {
+            return PrestamoActivo;
+        }
+
+        public void SetDevolucion(DateTime fd)
+        {
+            FechaDevolucion = fd;
+            PrestamoActivo = false;
+        }
+        
+
+
+    }
+}
